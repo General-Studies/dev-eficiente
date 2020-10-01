df85f3943f68a3994200bd78772c030ee04501e3
Jether Rodrigues <jetherrodrigues@gmail.com>
2020-09-30 22:07:39 -0300

    created initial version of challenge one of efficient developer journey

---
+++ one/src/main/kotlin/com/jetherrodrigues/challenge/one/OneApplication.kt
@@ -0,0 +1,11 @@
+package com.jetherrodrigues.challenge.one
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OneApplication
+
+fun main(args: Array<String>) {
+	runApplication<OneApplication>(*args)
+}
